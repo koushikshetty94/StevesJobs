30a7721401b8f6789653147957222e6a5bdf5e94
koushikshetty94 <koushikshetty94@gmail.com>
2020-06-25 20:19:16 +0530

    firdt commit

---
+++ app.js
@@ -0,0 +1,102 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+const expressStaticGzip = require("express-static-gzip");
+
+var skillsData = require("./modules/skillsData.json");
+const indexRouter = require("./routes/index");
+var employersRouter = require("./routes/employers/index");
+var candidatesRouter = require("./routes/candidates/index");
+var Skill = require("./models/skills");
+//dotenv
+require("dotenv").config();
+
+var app = express();
+
+const gzipOptions = {
+  enableBrotli: true,
+  orderPreference: ["br", "gz"],
+  setHeaders: function (res, path) {
+    res.setHeader("Cache-Control", "public, max-age=31536000");
+  },
+};
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  "/bundle",
+  expressStaticGzip(path.join(__dirname, "dist/bundle"), gzipOptions)
+);
+
+
+// Webpack Configuration
+if (process.env.NODE_ENV === "development") {
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config");
+  const compiler = webpack(webpackConfig);
+
+  app.use(
+    require("webpack-dev-middleware")(compiler, {
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    })
+  );
+
+  app.use(require("webpack-hot-middleware")(compiler));
+}
+
+
+//connect to database
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    console.log(err ? err : "db connected ");
+    if (!err) {
+      var skillsArray = skillsData.skills.map(s => {
+        return { name: s };
+      });
+      Skill.find({}, (err, findSkills) => {
+        if (err) return console.log(err);
+        if (findSkills.length === 0) {
+          Skill.insertMany(skillsArray, (error, skills) => {
+            if (error) return console.log(error);
+            console.log(skills);
+          });
+        }
+      });
+    }
+  }
+);
+
+app.use("/api/v1/employers", employersRouter);
+app.use("/api/v1/candidates", candidatesRouter);
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
---
+++ client/src/App.js
@@ -0,0 +1,151 @@
+import React from "react";
+import LandingPage from "./components/LandingPage/LandingPage";
+import Header from "./components/Common/Header";
+import CandidatesSignUp from "./components/CandidatesSignUp/CandidatesSignUp";
+import CandidatesLogin from "./components/CandidatesLogin/CandidatesLogin";
+import EmployersLogin from "./components/EmployersLogin/EmployersLogin";
+import EmployersSignUp from "./components/EmployersSignup/EmployersSignUp";
+import CandidatesProfile from "./components/CandidatesOnboarding/CandidatesProfile";
+
+import { Switch, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import {
+  logoutCandidate,
+  loginCandidate,
+  loginEmployer,
+  logoutEmployer,
+  identifyLoggedUser
+} from "./store/actions";
+import "./App.css";
+import AwesomeComponent from "./components/Loader/Lodaer";
+
+function PublicRoutes(props) {
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/candidates/signup">
+          <CandidatesSignUp />
+        </Route>
+        <Route path="/employers/signup">
+          <EmployersSignUp />
+        </Route>
+        <Route path="/candidates/login">
+          <CandidatesLogin />
+        </Route>
+        <Route path="/employers/login">
+          <EmployersLogin />
+        </Route>
+        />
+        <Route path="*">
+          <h1
+            style={{
+              margin: "200px auto",
+              textAlign: "center",
+              color: "red"
+            }}
+          >
+            Page Not found
+          </h1>
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+function PrivateRoutes(props) {
+  return (
+    <>
+      <Header {...props} />
+      <Switch>
+        <Route path="/" exact>
+          <LandingPage />
+        </Route>
+        <Route path="/candidates/profile">
+          <CandidatesProfile />
+        </Route>
+        <Route path="*">
+          <h1
+            style={{
+              margin: "200px auto",
+              textAlign: "center",
+              color: "red"
+            }}
+          >
+            Page Not found
+          </h1>
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(identifyLoggedUser());
+  }
+
+  handleLogout = () => {
+    localStorage.clear();
+    if (this.props.candidate.currentCandidate) {
+      this.props.dispatch(
+        logoutCandidate({
+          currentCandidate: null,
+          isAuthInProgress: false,
+          isAuthDone: false
+        })
+      );
+    } else if (this.props.employer.currentEmployer) {
+      this.props.dispatch(
+        logoutEmployer({
+          currentEmployer: null,
+          isAuthInProgress: false,
+          isAuthDone: false
+        })
+      );
+    }
+  };
+
+  loginFunction = () => {
+    if (this.props.candidateData) {
+      this.props.dispatch(
+        loginCandidate({
+          currentEmployer: null,
+          isAuthInProgress: false,
+          isAuthDone: false
+        })
+      );
+    } else if (this.props.employerData) {
+      this.props.dispatch(loginEmployer({ isEmployerLogged: true }));
+    }
+  };
+
+  render() {
+    let employer = this.props.employer;
+    let candidate = this.props.candidate;
+
+    return (
+      <>
+        {this.props.candidate.isAuthInProgress ||
+        this.props.employer.isAuthInProgress ? (
+          <AwesomeComponent />
+        ) : candidate.currentCandidate || employer.currentEmployer ? (
+          <PrivateRoutes
+            currentCandidate={candidate.currentCandidate}
+            handleLogout={this.handleLogout}
+            currentEmployer={employer.currentEmployer}
+          />
+        ) : (
+          <PublicRoutes />
+        )}
+      </>
+    );
+  }
+}
+
+function mapToProps({ candidate, employer }) {
+  return { candidate, employer };
+}
+
+export default connect(mapToProps)(App);
